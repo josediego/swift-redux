31f5b7a3352d40c83cdc558c6e0060ffbf091de5
Jose Diego <cardenas.josediego@gmail.com>
2019-11-14 01:12:19 -0500

    Initial Commit

---
+++ reduxpoc/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  reduxpoc
+//
+//  Created by Jose Diego on 11/14/19.
+//  Copyright © 2019 Focus in Lab. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
